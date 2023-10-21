b719e89570486e7f7356780efb0df35100168ca8
108900299+rawdatafeel@users.noreply.github.com <108900299+rawdatafeel@users.noreply.github.com>
2023-10-21 18:59:55 -0400

    Assets * Only copy contents of Assets folder instead of the entire folder

--- supermodel-patch.sh
+++ supermodel-patch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.supermodel/Analysis $HOME/.supermodel/Log $HOME/.supermodel/NVRAM $HOME/.supermodel/Config
+mkdir -p $HOME/.supermodel/Analysis $HOME/.supermodel/Log $HOME/.supermodel/NVRAM $HOME/.supermodel/Config $HOME/.supermodel/Assets
+# Only copy Games.xml, this file isn't expected to be edited by users.
 cp /app/bin/Config/Games.xml $HOME/.supermodel/Config
-cp -R /app/bin/Assets /app/bin/Docs $HOME/.supermodel
+cp -R /app/bin/Docs $HOME/.supermodel
+# Users can add custom assets to this folder, only copy contents instead of copying the entire folder.
+cp -R /app/bin/Assets/* $HOME/.supermodel/Assets
 
 if [ ! -f $HOME/.supermodel/Config/Supermodel.ini ]; then
 
